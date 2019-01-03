9d5ed32c842f17807680423755218db2893ef6ae
Hafeez <feeziman007@hotmail.com>
2019-01-03 20:01:16 +1100

    styles added to AppBar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,18 +1,41 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { toProperCase } from "../utils";
 
 const Bar = styled.div`
   display: grid;
   grid-template-columns: 180px auto 100px 100px;
+  margin-bottom: 40px;
 `;
+
+const Logo = styled.div`
+  font-size: 1.5em;
+`;
+
+const ControlButtonElement = styled.div`
+  cursor: pointer;
+  ${props =>
+    props.active &&
+    css`
+      text-shadow: 0 0 60px #03ff03;
+    `}
+`;
+
+function ControlButton({ name, active }) {
+  return (
+    <ControlButtonElement active={active}>
+      {toProperCase(name)}
+    </ControlButtonElement>
+  );
+}
 
 const AppBar = () => {
   return (
     <Bar>
-      <div>CryptoDash</div>
+      <Logo>CryptoDash</Logo>
       <div />
-      <div>Dashborad</div>
-      <div>Settings</div>
+      <ControlButton active name={"dashboard"} />
+      <ControlButton name={"settings"} />
     </Bar>
   );
 };
