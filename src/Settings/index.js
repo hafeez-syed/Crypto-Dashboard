e407e67237abe5066c30ac71355bbccc93891b4d
Hafeez <feeziman007@hotmail.com>
2019-01-07 13:56:31 +1100

    Page wrapper added

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Welcome from "./Welcome";
 import ConfirmButton from "./ConfirmButton";
+import Page from "../Shared/Page";
 
 const Settings = () => {
   return (
-    <div>
+    <Page name="settings">
       <Welcome />
       <ConfirmButton />
-    </div>
+    </Page>
   );
 };
 
